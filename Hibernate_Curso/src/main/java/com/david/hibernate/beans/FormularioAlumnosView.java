5b25925dfaa14cb0da342d0d64492eefb267731b
David <cristhegood15@gmail.com>
2022-11-14 13:39:51 -0600

    -correciones

--- Hibernate_Curso/src/main/java/com/david/hibernate/beans/FormularioAlumnosView.java
+++ Hibernate_Curso/src/main/java/com/david/hibernate/beans/FormularioAlumnosView.java
@@ -5,60 +5,57 @@ import java.io.Serializable;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.faces.bean.ManagedBean;
-import javax.faces.view.ViewScoped;
+import javax.faces.bean.RequestScoped;
 
 import com.david.hibernate.dao.AlumnoDAO;
 import com.david.hibernate.entidades.Alumno;
 
-
 @ManagedBean(name = "insertaAlumno")
-@ViewScoped
+@RequestScoped
 public class FormularioAlumnosView implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	//variables
+
+	// variables
 	private Alumno alumno;
 	private AlumnoDAO servicioAlumno;
-	private boolean accionHecha; 
+	private boolean accionHecha;
 
-	
 	@PostConstruct
 	public void inicia() {
 		System.out.println("--Inicia Formulario Alumno");
-		alumno=new Alumno();
-		
-		servicioAlumno= new AlumnoDAO();
+		alumno = new Alumno();
+
+		servicioAlumno = new AlumnoDAO();
 	}
-	
+
 	@PreDestroy
 	public void termina() {
 		System.out.println("--Termina Formulario Alumno");
-		alumno=null;
-		servicioAlumno=null;
+		alumno = null;
+		servicioAlumno = null;
 	}
-	
-	
-	//metodos
+
+	// metodos
 	public void insertarAlumno() {
-		
-		System.out.println("Alumno: "+alumno);
-		//servicioAlumno.insertar(alumno);
-		
+
+		System.out.println("Alumno: " + alumno);
+		// servicioAlumno.insertar(alumno);
+
 	}
-	
+
 	public void actualizarAlumno() {
 		servicioAlumno.actualizar(alumno);
 	}
-	
+
 	public void eliminarAlumno() {
 		servicioAlumno.eliminar(alumno);
 	}
-	
-	
-	//getters and setters
+
+	// getters and setters
 	public Alumno getAlumno() {
 		return alumno;
 	}
+
 	public void setAlumno(Alumno alumno) {
 		this.alumno = alumno;
 	}
@@ -70,5 +67,5 @@ public class FormularioAlumnosView implements Serializable {
 	public void setAccionHecha(boolean accionHecha) {
 		this.accionHecha = accionHecha;
 	}
-	
+
 }
